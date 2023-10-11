2bb1687b02b892353a313c22359db5631fa552f8
zartinn <martin.zachoszcz@googlemail.com>
2023-10-11 17:06:26 +0200

    feat: refactor card component with its different content types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Card } from './Card/Card.astro';
+export { default as CardLink } from './Card/CardLink.astro';
 export { default as Header } from './Header/Header.astro';
 export { default as Link } from './Link/Link.astro';
 export { default as LangLink } from './LangLink/LangLink.astro';
