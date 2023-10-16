0e14035b7da8379c4204b27b724b84c35041233c
zartinn <martin.zachoszcz@googlemail.com>
2023-10-16 16:26:28 +0200

    feat: improve timeline animation logic

---
+++ src/assets/images/journey/index.ts
@@ -0,0 +1,19 @@
+import berlin from './berlin.png';
+import highschool from './high-school.png';
+import siemens from './siemens-professional-education.png';
+import psychology from './psychology.png';
+import humanFactors from './human-factors.png';
+import cariad from './cariad.png';
+import freelancing from './freelancing.png';
+
+const images = [
+  { img: berlin, alt: "vibrant scenery of Berlin" },
+  { img: highschool, alt: "highschool graduation" },
+  { img: siemens, alt: "siemens professional education" },
+  { img: psychology, alt: "Psychology study" },
+  { img: humanFactors, alt: "Masters study" },
+  { img: cariad, alt: "my work at Cariad" },
+  { img: freelancing, alt: "Freelancing endovour" }
+];
+
+export default images;
