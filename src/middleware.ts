1de3fb60caecfdedd26faa0a96e614543dea8814
zartinn <martin.zachoszcz@googlemail.com>
2023-10-24 11:28:51 +0200

    feat: disable german version for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,8 +23,8 @@ export const onRequest = defineMiddleware((context, next) => {
   const requestUrl = new URL(context.url);
   const urlLang = requestUrl.pathname.split('/')[1] || '';
   // If the URL doesn't contain a language, redirect to defaultLang
-  if (urlLang === 'en') {
-    const redirectURL = `${requestUrl.pathname.replace('/en', '/')}`.replace(/\/+/g, '/');
+  if (urlLang === 'en' || urlLang === 'de') {
+    const redirectURL = `${requestUrl.pathname.replace(`/${urlLang}`, '/')}`.replace(/\/+/g, '/');
     return context.redirect(redirectURL);
   }
   // Continue with the next middleware or route handler
