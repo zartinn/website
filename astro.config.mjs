c1dd8924854dd83afa176b753dfd83a65b0cdc3b
zartinn <martin.zachoszcz@googlemail.com>
2023-12-20 22:07:41 +0100

    chore: set output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   prefetch: {
     defaultStrategy: 'viewport'
   },
-  output: 'hybrid',
+  output: 'server',
   integrations: [tailwind()],
   build: {
     inlineStylesheets: 'never'
