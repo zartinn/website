03d89a3fe902f1c32910215800d7fe0a89dbc659
zartinn <martin.zachoszcz@googlemail.com>
2023-12-20 16:04:30 +0100

    fix: pages folder by extracting ts files to different location

--- src/pages/[lang]/pages.ts
+++
@@ -1,5 +0,0 @@
-export { default as Contact } from './contact/index.astro';
-export { default as Feats } from './feats/index.astro';
-export { default as Journey } from './journey/index.astro';
-export { default as Craft } from './craft/index.astro';
-export { default as Home } from './index.astro';
