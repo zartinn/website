1d7e7159bf01d945ea1ab4b1a5708eed155c6d7e
zartinn <martin.zachoszcz@googlemail.com>
2023-12-20 22:10:27 +0100

    feat: activate middleware again

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,38 +7,28 @@ const AUTH_STRING = `Basic ${Buffer.from(`${BASIC_AUTH_USER}:${BASIC_AUTH_PASS}`
 const paths = ['/', '/contact', '/feats', '/journey', '/craft'];
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {  
-  return next();
-  // const authHeader = context.request.headers.get('authorization') || '';
-  // // Check for Basic Auth
-  // if (BASIC_AUTH_USER && authHeader !== AUTH_STRING) {
-  //   return new Response('Unauthorized', {
-  //     headers: {
-  //       'WWW-Authenticate': 'Basic realm="Protected Area"'
-  //     },
-  //     status: 401
-  //   })
-  // }
-
-  // Extract current language from URL
-  const requestUrl = new URL(context.url);
-  const urlLang = requestUrl.pathname.split('/')[1] || '';
-  // If the URL doesn't contain a language, redirect to defaultLang
-  // if (urlLang === 'en' || urlLang === 'de') {
-  //   const redirectURL = `${requestUrl.pathname.replace(`/${urlLang}`, '/')}`.replace(/\/+/g, '/');
-  //   return context.redirect(redirectURL);
-  // }
+  const authHeader = context.request.headers.get('authorization') || '';
+  // Check for Basic Auth
+  if (BASIC_AUTH_USER && authHeader !== AUTH_STRING) {
+    return new Response('Unauthorized', {
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Protected Area"'
+      },
+      status: 401
+    })
+  }
 
   // Continue with the next middleware or route handler
-  // if (paths.includes(context.url.pathname) && context.request.method === 'GET') {
-  //   const res = await next();
-  //   const clonedRes = res.clone();
-  //   const body = await clonedRes.arrayBuffer();
-  //   const etag = await generateETag(body);
-  //   res.headers.set('Etag', etag);
-  //   return res
-  // } else {
-  //   return next();
-  // }
+  if (paths.includes(context.url.pathname) && context.request.method === 'GET') {
+    const res = await next();
+    const clonedRes = res.clone();
+    const body = await clonedRes.arrayBuffer();
+    const etag = await generateETag(body);
+    res.headers.set('Etag', etag);
+    return res
+  } else {
+    return next();
+  }
 });
 
 async function generateETag(content: ArrayBuffer) {
